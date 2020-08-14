75f8cb5aca91569366090171a07a5998b25fdca7
XuYanhang <69621133+object1314@users.noreply.github.com>
2020-08-14 18:38:26 +0800

    Create package-info.java

---
+++ src/main/java/org/xuyh/container/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 XuYanhang
+ * 
+ */
+/**
+ * There provide the containers to manage some objects.
+ * 
+ * @author XuYanhang
+ * @since 2020-08-14
+ */
+package org.xuyh.container;
