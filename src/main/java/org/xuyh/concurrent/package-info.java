3aa3231660ef38212df4f82ab1de57390ba2a73b
XuYanhang <69621133+object1314@users.noreply.github.com>
2020-08-15 11:03:29 +0800

    Create package-info.java

---
+++ src/main/java/org/xuyh/concurrent/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 XuYanhang
+ * 
+ */
+/**
+ * There provide some tools on concurrent environment.
+ * 
+ * @author XuYanhang
+ * @since 2020-08-15
+ */
+package org.xuyh.concurrent;
