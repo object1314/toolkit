b917291d0754fe948ec8db7b36c4e4b9981c76b1
XuYanhang <69621133+object1314@users.noreply.github.com>
2020-10-26 19:34:13 +0800

    Update package-info.java

--- src/main/java/org/xuyh/net/package-info.java
+++ src/main/java/org/xuyh/net/package-info.java
@@ -1,8 +1,12 @@
-/**
+/*
+ * Copyright (c) 2020 XuYanhang
  * 
  */
 /**
- * @author Administrator
+ * Toolkits on network relative operations.
+ * 
+ * @author XuYanhang
+ * @since 2020-10-26
  *
  */
 package org.xuyh.net;
